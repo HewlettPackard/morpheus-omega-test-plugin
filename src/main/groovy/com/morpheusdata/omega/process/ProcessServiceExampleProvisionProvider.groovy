b731adacf1cfc009f7025b0317dfde01915c71a1
Mike Carlin <mike@mcarlin.com>
2025-05-20 12:44:15 -0700

    process-service: ensure we append to output when ending

--- src/main/groovy/com/morpheusdata/omega/process/ProcessServiceExampleProvisionProvider.groovy
+++ src/main/groovy/com/morpheusdata/omega/process/ProcessServiceExampleProvisionProvider.groovy
@@ -167,11 +167,11 @@ class ProcessServiceExampleProvisionProvider extends AbstractProvisionProvider i
 		switch (exampleToRun) {
 			case ProcessExample.BASIC:
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: ProcessStepType.EXECUTE_ACTION), "").blockingGet()
-				context.async.process.endProcessStep(workloadRequest.process, "complete", "basic output").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "complete", "basic output", true).blockingGet()
 				break
 			case ProcessExample.BASIC_CUSTOM:
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: CUSTOM_STEP_TYPE), "").blockingGet()
-				context.async.process.endProcessStep(workloadRequest.process, "complete", "basic custom output").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "complete", "basic custom output", true).blockingGet()
 				break
 			case ProcessExample.UPDATE:
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: CUSTOM_STEP_TYPE), "").blockingGet()
@@ -188,7 +188,7 @@ class ProcessServiceExampleProvisionProvider extends AbstractProvisionProvider i
 
 					sleep(increment)
 				}
-				context.async.process.endProcessStep(workloadRequest.process, "complete", "final").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "complete", "final", true).blockingGet()
 				break
 			case ProcessExample.FAIL:
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: CUSTOM_STEP_TYPE), "").blockingGet()
@@ -196,13 +196,13 @@ class ProcessServiceExampleProvisionProvider extends AbstractProvisionProvider i
 						error: "failure by design\n",
 				)
 				context.async.process.updateProcessStep(workloadRequest.process, CUSTOM_STEP_TYPE, update, true).blockingGet()
-				context.async.process.endProcessStep(workloadRequest.process, "failed", "final").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "failed", "final", true).blockingGet()
 				break
 			case ProcessExample.MULTI:
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: CUSTOM_STEP_FIRST_TYPE), "").blockingGet()
-				context.async.process.endProcessStep(workloadRequest.process, "complete", "final first").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "complete", "final first", true).blockingGet()
 				context.async.process.startProcessStep(workloadRequest.process, new ProcessEvent(stepType: CUSTOM_STEP_SECOND_TYPE), "").blockingGet()
-				context.async.process.endProcessStep(workloadRequest.process, "complete", "final second").blockingGet()
+				context.async.process.endProcessStep(workloadRequest.process, "complete", "final second", true).blockingGet()
 				break
 		}
 
